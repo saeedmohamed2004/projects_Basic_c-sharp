b0f83107690284cee438b72193607db6ba9f6ecf
saeed mohamed <115119496+saeedmohamed2004@users.noreply.github.com>
2026-02-13 18:21:50 +0200

    Add files via upload

---
+++ Quiz Game/Quiz Game/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Quiz_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] questions = new string[]
+            {
+                "1. what is capital of italy ?",
+                "2. what is the red planet ?",
+                "3. what is the largest animal ?"
+            };
+            string[] answers = new string[]
+            {
+                "rome",
+                "mars",
+                "whale"
+            };
+            int correctanswer = 0;
+
+            Console.WriteLine("welcome to the game");
+            Console.WriteLine("please answer the following qestions");
+            Console.WriteLine("---------");
+           
+            for (int i = 0; i < questions.Length; i++)
+            {
+                Console.WriteLine(questions[i]);
+                string useranswer = Console.ReadLine();
+                try
+                {
+                    bool result = istheanswercorrect(useranswer, answers[i]);
+                    if (result == true)
+                    {
+                        Console.WriteLine("the answer is correct !");
+                        correctanswer ++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"sorry try again , the corrcti answer is {answers[i]}");
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+
+            }
+            Console.WriteLine($"your score is {correctanswer}/3");
+
+            Console.WriteLine("quiz completed thank you for playing ");
+
+
+        }
+         private static bool istheanswercorrect(string userinput , string coorectanswer) 
+        {
+            if (string.IsNullOrEmpty(userinput))
+            {
+                throw new Exception("answer cant be empty");
+            }
+            //if (userinput == null) -----------> "  "فاضيه
+            //{
+            //    throw new Exception("answer cant be empty");
+
+            //}
+            if (userinput == coorectanswer) 
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+    }
+}
